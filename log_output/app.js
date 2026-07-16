6f63a82e07bd1d007c5bb0e048372c46b04d9946
Andrii Tsyren <andrii_tsyren@epam.com>
2026-07-16 13:40:54 +0300

    Add status endpoint, service and Ingress for log_output

--- log_output/app.js
+++ log_output/app.js
@@ -1,7 +1,16 @@
+const http = require('http');
 const { randomUUID } = require('crypto');
 
+const PORT = process.env.PORT || 3000;
 const id = randomUUID();
 
 setInterval(() => {
   console.log(`${id}: ${new Date().toISOString()}`);
 }, 5000);
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`${id}: ${new Date().toISOString()}`);
+  })
+  .listen(PORT);
